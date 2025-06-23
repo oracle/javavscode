678e94ddb12bd38aae8b8c33e4af154c5420521c
Shivam Madan <shivam.madan@oracle.com>
2025-06-23 17:28:01 +0530

    Changes to support configurations and handle the async tasks.

--- nbcode/notebooks/src/org/netbeans/modules/nbcode/java/notebook/NotebookConfigs.java
+++ nbcode/notebooks/src/org/netbeans/modules/nbcode/java/notebook/NotebookConfigs.java
@@ -15,25 +15,64 @@
  */
 package org.netbeans.modules.nbcode.java.notebook;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import org.eclipse.lsp4j.ConfigurationItem;
 import org.eclipse.lsp4j.ConfigurationParams;
 import org.netbeans.modules.java.lsp.server.protocol.NbCodeLanguageClient;
-
+import org.openide.util.Exceptions;
+import java.lang.Void;
 /**
  *
  * @author atalati
  */
 public class NotebookConfigs {
 
-    private static final String NOTEBOOK_JDK_HOME = "notebook.jdkhome";
-    private String jdkVersion = null;
+//    private static final String NOTEBOOK_JDK_HOME = "notebook.jdkhome";
+//    private String jdkVersion = null;
+    
+    private static final String[] NOTEBOOK_CONFIG_LABELS={"notebook.classpath","notebook.modulepath","notebook.addmodules","notebook.enablePreview","notebook.implicitImports"};
     private NbCodeLanguageClient client = null;
+    private String classPath = null;
+    private String modulePath = null;
+    private String addModules = null;
+    private boolean enablePreview = false;
+    private List<String> implicitImports = null;
+    private CompletableFuture<Void> initialized;
+
+    public CompletableFuture<Void> getInitialized() {
+        return initialized;
+    }
+    
+
+    public String getClassPath() {
+        return classPath;
+    }
+
+    public String getModulePath() {
+        return modulePath;
+    }
+
+    public String getAddModules() {
+        return addModules;
+    }
+
+    public boolean isEnablePreview() {
+        return enablePreview;
+    }
+
+    public List<String> getImplicitImports() {
+        return implicitImports;
+    }
+    
 
     private NotebookConfigs() {
+        
     }
 
     public static NotebookConfigs getInstance() {
@@ -47,36 +86,80 @@ public class NotebookConfigs {
 
     public void setLanguageClient(NbCodeLanguageClient client) {
         this.client = client;
+        
+        try {
+            this.initialized = initializeConfigs();
+        } catch (InterruptedException | ExecutionException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 
     public NbCodeLanguageClient getLanguageClient() {
         return client;
     }
-
-    public String getJdkVersion() throws InterruptedException, ExecutionException {
-        // Figure out how to get Java major version from the path to jdk home
-        // Option-1: Run java --version in a different process and get it's output.
-        // Option-2: Check release file in the home path it might have info about major version.
-        String notebookJdkVersion = jdkVersion != null ? jdkVersion : null;
-        if (notebookJdkVersion != null && client != null) {
-            if (client != null) {
-                ConfigurationItem configItem = new ConfigurationItem();
-                configItem.setSection(client.getNbCodeCapabilities().getConfigurationPrefix() + NOTEBOOK_JDK_HOME);
-                notebookJdkVersion = client.configuration(new ConfigurationParams(List.of(configItem))).thenApply((c) -> {
-                    if (c != null) {
-                        return ((JsonPrimitive) c.get(0)).getAsString();
-                    }
-                    return null;
-                }).get();
-                if (notebookJdkVersion != null) {
-                    this.jdkVersion = notebookJdkVersion;
-                }
-            }
+    private List<ConfigurationItem> getConfigItems(){
+        List<ConfigurationItem> items = new ArrayList<>();
+        for(String label:NOTEBOOK_CONFIG_LABELS){
+            ConfigurationItem item = new ConfigurationItem();
+            item.setSection(client.getNbCodeCapabilities().getConfigurationPrefix() + label);
+            items.add(item);
         }
-        return notebookJdkVersion;
+        return items;
     }
+    private CompletableFuture<Void> initializeConfigs() throws InterruptedException, ExecutionException{
+        if (client != null) {
+        
+          CompletableFuture<List<Object>> configValues = client.configuration(new ConfigurationParams(getConfigItems()));
+         return configValues.thenAccept((c)->{
+             if(c!=null){
+                 if(c.get(0)!=null){
+                     classPath = ((JsonPrimitive) c.get(0)).getAsString();
+                 }
+                 if(c.get(1)!=null){
+                     modulePath = ((JsonPrimitive) c.get(1)).getAsString();
+                 }
+                 if(c.get(2)!=null){
+                     addModules = ((JsonPrimitive) c.get(2)).getAsString();
+                 }
+                 if(c.get(3)!=null){
+                     enablePreview = ((JsonPrimitive) c.get(3)).getAsBoolean();
+                 }
+                 if(c.get(4)!=null){
+                    implicitImports =  ((JsonArray) c.get(4)).asList().stream().map((elem)->elem.getAsString()).toList();
+                             
+                 }
+             }
+         });
+                 
+     }
+        return CompletableFuture.completedFuture(null);
+        
+    }
+//    public String getJdkVersion() throws InterruptedException, ExecutionException {
+//        // Figure out how to get Java major version from the path to jdk home
+//        // Option-1: Run java --version in a different process and get it's output.
+//        // Option-2: Check release file in the home path it might have info about major version.
+//        String notebookJdkVersion = jdkVersion != null ? jdkVersion : null;
+//        if (notebookJdkVersion != null && client != null) {
+//            if (client != null) {
+//                ConfigurationItem configItem = new ConfigurationItem();
+//                configItem.setSection(client.getNbCodeCapabilities().getConfigurationPrefix() + NOTEBOOK_JDK_HOME);
+//                notebookJdkVersion = client.configuration(new ConfigurationParams(List.of(configItem))).thenApply((c) -> {
+//                    if (c != null) {
+//                        return ((JsonPrimitive) c.get(0)).getAsString();
+//                    }
+//                    return null;
+//                }).get();
+//                if (notebookJdkVersion != null) {
+//                    this.jdkVersion = notebookJdkVersion;
+//                }
+//            }
+//        }
+//        return notebookJdkVersion;
+//    }
 
     public void notebookConfigsChangeListener(JsonObject settings) {
         // depends on #8514 PR open in Netbeans
+        
     }
 }
